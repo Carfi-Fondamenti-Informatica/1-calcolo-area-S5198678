ab194a9957b44f718a9620f8483535e0770b7f90
S5198678 <100881826+S5198678@users.noreply.github.com>
2022-03-14 20:19:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,19 +4,19 @@ using namespace std;
 int main() {
     int a,b,c;
     float areaR,areaT1,areaS,areaT2;
-      cin >> a; cin >> b;  cin >> c;
     cout << "Inserire il valore di a"<<endl;
+    cin >> a;
     cout << "Inserire il valore di b"<<endl;
+    cin >> b;
     cout << "Inserire il valore di c"<<endl;
+    cin >> c;
     areaR = (float) (a*b);
     areaT1 = (float) (a*b)/2;
     areaS = (float) (a^2);
     areaT2 = (float) (0.5*(a+b)*c);
-
     cout << "il valore di a è : "<< a <<endl;
     cout << "il valore di b è : "<< b <<endl;
     cout << "il valore di c è : "<< c <<endl;
-
     cout << "area di rettangolo  "<< areaR<<endl;
     cout << "area di triangolo  "<< areaT1 <<endl;
     cout << "area di quadrato  "<< areaS <<endl;
